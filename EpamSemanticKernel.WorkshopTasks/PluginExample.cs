9d2975a2925ef949c570b85cabee3f58f782f883
dmitrycx <dkolotsey@gmail.com>
2023-12-17 12:29:12 +0300

    add custom plugin usage

--- EpamSemanticKernel.WorkshopTasks/PluginExample.cs
+++ EpamSemanticKernel.WorkshopTasks/PluginExample.cs
@@ -14,7 +14,7 @@ public class PluginExample
     {
         var builder = new KernelBuilder();
 
-        // Prompt
+        // default plugin
         var (model1, azureEndpoint1, apiKey1, gpt35TurboServiceId) = Settings.LoadFromFile(model: "gpt-35-turbo");
         builder.AddAzureOpenAIChatCompletion(model1, model1, azureEndpoint1, apiKey1, serviceId: gpt35TurboServiceId);
 
@@ -43,5 +43,45 @@ public class PluginExample
         var result = await kernel.InvokeAsync(timeFunctions["daysAgo"], arguments);
 
         Console.WriteLine(result);
+        
+        // custom plugin
+        var customPluginFunctions = kernel.ImportPluginFromObject(new CustomPlugin(kernel, serviceId: gpt4ServiceId));
+        Console.WriteLine($"{string.Join("\n", customPluginFunctions.Select(_ => $"[{_.Name}] : {_.Description}"))}");
+
+        Func<string, string, Task> TranslateAsync = async (string input, string lang) =>
+        {
+            // Save new message in the context variables
+            arguments["input"] = input;
+            arguments["lang"] = lang;
+
+            var answer = await customPluginFunctions["Translate"].InvokeAsync(kernel, arguments);
+        };
+
+        arguments["history"] = string.Empty;
+
+        Func<string, Task> ChatAsync = async (string input) =>
+        {
+            // Save new message in the context variables
+            arguments["message"] = input;
+
+            var answer = await customPluginFunctions["Chat"].InvokeAsync(kernel, arguments);
+        };
+
+        // Set specific arguments for the findBooks function
+        arguments.Add("BooksNumber", "10");
+        arguments.Add("YearFrom", "1900");
+        arguments.Add("YearTo", "2000");
+        
+        var sourceInput = "Hi, I'm looking for the best historical book suggestions top-{{$BooksNumber}} from {{$YearFrom}} to {{$YearTo}}";
+
+        await TranslateAsync(sourceInput, "Italian");
+        await TranslateAsync(sourceInput, "Russian");
+        await TranslateAsync(sourceInput, "Chinese");
+
+        await ChatAsync(sourceInput);
+        await ChatAsync("And from these books find top-10 the most interesting facts");
+        await ChatAsync("Describe he main idea of selected facts");
+
+        Console.WriteLine($"[Chat History]: {arguments["history"]}");
     }
 }
